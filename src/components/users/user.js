c5513d179075b2321725f58968616057e52935dc
Fadi NOUFAL <noufal4me@gmail.com>
2019-06-12 09:51:54 +0200

    Update user css

--- src/components/users/user.js
+++ src/components/users/user.js
@@ -5,7 +5,7 @@ export default function (props) {
     const SimpleList = () => (
         <div className="user-list">
             {props.list.map(function (item) {
-                return <div key={item.id}>
+                return <div key={item.id} className="user-list-element">
                     <ul >
                         <li>{item.id}</li>
                         <li>{item.firstname}</li>
