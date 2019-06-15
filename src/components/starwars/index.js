1092549f631f7cfceaa504785ff065e046b2e6ba
Fadi NOUFAL <noufal4me@gmail.com>
2019-06-15 14:32:02 +0200

    Import people list

--- src/components/starwars/index.js
+++ src/components/starwars/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import {logicalExpression} from "@babel/types";
 
 export default class Starwars extends React.Component {
+
     constructor(props) {
         super(props)
-
         this.state = {
-
+            apiUrl:'https://swapi.co/api/people/'
         }
+    }
+
+    componentDidMount() {
+        console.log('did mount')
     }
 
     render() {
